f0ed07cab8f0e00c1f2cfd2f3a14580cd972c0e2
Tran Minh Nhut <minhnhutwork2501@gmail.com>
2023-06-08 19:33:37 +0700

    first commit

---
+++ src/main/java/com/cybersoft/cozastore_21/Cozastore21Application.java
@@ -0,0 +1,13 @@
+package com.cybersoft.cozastore_21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cozastore21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cozastore21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersoft/cozastore_21/Cozastore21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersoft.cozastore_21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cozastore21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
